524322506e578cc3bec570e47fd681dc49831b59
saeed94 <saeed.hajizade@ymail.com>
2018-09-30 13:05:17 +0330

    001-commit

---
+++ src/com/codebehind/App.java
@@ -0,0 +1,43 @@
+package com.codebehind;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JButton Button1;
+    private JPanel jPanel;
+    private JLabel lable1;
+    private JButton ButtonExit;
+
+    public static void main(String[] args) {
+        JFrame jFrame=new JFrame("برنامه من");
+        jFrame.setPreferredSize(new Dimension(600,400));
+        jFrame.setContentPane(new App().jPanel);
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setVisible(true);
+        jFrame.setResizable(false);
+
+    }
+
+    public App() {
+        Button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "خوش آمدید");
+                lable1.setText("خوش آمدید");
+            }
+        });
+        ButtonExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               System.exit(0);
+               //exit
+            }
+        });
+    }
+
+
+}
