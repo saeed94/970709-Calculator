24617bfa16c90e3bdefbb7a3bf7b84602311b173
saeed94 <saeed.hajizade@ymail.com>
2018-10-02 22:18:14 +0330

    003-Calculator

--- src/com/codebehind/App.java
+++ src/com/codebehind/App.java
@@ -5,19 +5,17 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class App  {
-    private JButton Button1;
+public class App {
     private JPanel jPanel;
-    private JLabel lable1;
-    private JButton ButtonExit;
-    private JButton button2;
-    private JButton button3;
+    private JButton buttonCalc;
+    private JButton buttonInfo;
+    private JButton buttonExit;
     static JFrame jFrame;
     static JFrame jFrame2;
 
     public static void main(String[] args) {
-        jFrame=new JFrame("برنامه من");
-        jFrame.setPreferredSize(new Dimension(600,400));
+        jFrame = new JFrame("برنامه من");
+        jFrame.setPreferredSize(new Dimension(440, 200));
         jFrame.setContentPane(new App().jPanel);
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.pack();
@@ -28,26 +26,25 @@ public class App  {
     }
 
     public App() {
-        Button1.addActionListener(new ActionListener() {
+        buttonInfo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "خوش آمدید");
-                lable1.setText("خوش آمدید");
+                JOptionPane.showMessageDialog(null, "برنامه ماشین حساب ساده نوشته شده با زبان جاوا");
             }
         });
-        ButtonExit.addActionListener(new ActionListener() {
+        buttonExit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-               System.exit(0);
-               //exit
+                System.exit(0);
+                //exit
             }
         });
-        button3.addActionListener(new ActionListener() {
+        buttonCalc.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                jFrame2=new JFrame("22222222");
-                jFrame2.setPreferredSize(new Dimension(500,300));
+                jFrame2 = new JFrame("22222222");
+                jFrame2.setPreferredSize(new Dimension(320, 450));
                 jFrame2.setContentPane(new Form2().jPanel);
                 jFrame2.setDefaultCloseOperation(jFrame2.HIDE_ON_CLOSE);
                 jFrame2.pack();
@@ -57,6 +54,7 @@ public class App  {
             }
         });
     }
+
 
 
 }
--- src/com/codebehind/Form2.java
+++ src/com/codebehind/Form2.java
@@ -1,29 +1,130 @@
 package com.codebehind;
 
-import sun.swing.SwingUtilities2;
-
 import javax.swing.*;
-import javax.swing.border.TitledBorder;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Form2 extends JFrame{
-    private JButton button1;
+public class Form2 extends JFrame {
+    private JButton buttonBackSpace;
     public JPanel jPanel;
-//    static JFrame jFrame;
+    private JButton buttonClear;
+    private JButton buttonClears;
+    private JButton buttonCalculate;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button0;
+    private JButton buttonDivision;
+    private JButton buttonMultiplication;
+    private JButton buttonSubtraction;
+    private JButton buttonPoint;
+    private JButton buttonSum;
+    private JLabel lableResult;
+    private JLabel lableCalc;
+    //    static JFrame jFrame;
+
+    double result=0;
 
 
+    public Form2() {
 
-    public Form2()  {
-
+        button0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText(lableResult.getText()+"0");
+            }
+        });
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                button1.setText("jhjgkffdh");
-                setTitle("sldfkjsjlf");
+                lableResult.setText(lableResult.getText()+"1");
             }
         });
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText(lableResult.getText()+"2");
+            }
+        });
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText(lableResult.getText()+"3");
+            }
+        });
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText(lableResult.getText()+"4");
+            }
+        });
+        button5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText(lableResult.getText()+"5");
+            }
+        });
+        button6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText(lableResult.getText()+"6");
+            }
+        });
+        button7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText(lableResult.getText()+"7");
+            }
+        });
+        button8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText(lableResult.getText()+"8");
+            }
+        });
+        button9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText(lableResult.getText()+"9");
+            }
+        });
+        buttonPoint.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText(lableResult.getText()+".");
+            }
+        });
+
+        buttonBackSpace.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int n=lableResult.getText().length();
+                String s =lableResult.getText().substring(0,n-1);
+                lableResult.setText(s);
+            }
+        });
+        buttonClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText("");
+            }
+        });
+        buttonClears.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lableResult.setText("");
+                lableCalc.setText("");
+            }
+        });
+
+
+
     }
 
 }
